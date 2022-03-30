c14b9d2899da7ab9ac77dbaa79f98c3317e3d9cf
Ronald Dobos <ronald.dobos0@gmail.com>
2022-03-30 17:02:11 +0200

    test

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,8 +1,8 @@
 export const GAME_TITLE = 'Laurine wordle <3'
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const WIN_MESSAGES = ['Good Job Fish!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters, but its oke fish'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
@@ -15,8 +15,8 @@ export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
   `Must use ${guess} in position ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
   `Guess must contain ${letter}`
-export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
+export const ENTER_TEXT = 'Stlac ma'
+export const DELETE_TEXT = 'Vymaz'
 export const STATISTICS_TITLE = 'Statistics'
 export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
 export const NEW_WORD_TEXT = 'New word in'
